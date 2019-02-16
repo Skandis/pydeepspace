765c13c303320ac11575a6d84573a5eb3b9ae074
Sam Kerrison <sam.kerrison@thedropbears.org.au>
2019-02-17 07:23:17 +1100

    Turn front auto to subclass

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -187,51 +187,18 @@ class LeftStartAuto(AutoBase):
     # DEFAULT = True
 
 
-class FrontAuto(AutonomousStateMachine):
-    MODE_NAME = "Front autonomous"
+class FrontAuto(AutoBase):
+    MODE_NAME = "Front Auto"
     DEFAULT = True
-    # Here magicbot injects components
-    hatch_deposit: HatchDepositAligner
-    hatch_intake: HatchIntakeAligner
-
-    chassis: SwerveChassis
-    hatch: Hatch
-    imu: NavX
-    vision: Vision
-
-    # This one is just a typehint
-    pursuit: PurePursuit
 
     def __init__(self):
         super().__init__()
         self.left_front_cargo_bay = Waypoint(
             5.6 - SwerveChassis.LENGTH / 2, 0.2, 0, 0.75
         )
-        self.setup_loading_bay = Waypoint(3.3, 3.3, math.pi, 2)
-        self.loading_bay = Waypoint(0.2 + SwerveChassis.LENGTH / 2, 3.4, math.pi, 1)
         self.right_front_cargo_bay = Waypoint(
-            5 - SwerveChassis.LENGTH / 2, -0.4, 0, 0.5
+            5.4 - SwerveChassis.LENGTH / 2, -0.4, 0, 0.5
         )
-        self.start_pos = Waypoint(
-            1.19 + SwerveChassis.LENGTH / 2, 0 + SwerveChassis.WIDTH / 2, 0, 2
-        )
-
-        self.completed_runs = 0
-        self.desired_angle = 0
-        self.desired_angle_navx = 0
-        self.minimum_path_completion = 0.85
-
-        self.acceleration = 1
-        self.deceleration = -0.5
-
-        self.pursuit = PurePursuit(look_ahead=0.2, look_ahead_speed_modifier=0.25)
-
-    def on_enable(self):
-        super().on_enable()
-        self.chassis.odometry_x = self.start_pos[0]
-        self.chassis.odometry_y = self.start_pos[1]
-        self.completed_runs = 0
-        # print(f"odometry = {self.current_pos}")
 
     @state(first=True)
     def drive_to_cargo_bay(self, initial_call):
@@ -265,76 +232,3 @@ class FrontAuto(AutonomousStateMachine):
             self.next_state("deposit_hatch")
             self.completed_runs += 1
 
-    @state
-    def deposit_hatch(self, initial_call):
-        if initial_call:
-            self.hatch_deposit.engage(initial_state="target_tape_align")
-        if not self.hatch.has_hatch:
-            self.next_state("drive_to_loading_bay")
-
-    @state
-    def drive_to_loading_bay(self, initial_call):
-        if initial_call:
-            if self.completed_runs == 1:
-                waypoints = insert_trapezoidal_waypoints(
-                    (
-                        self.current_pos,
-                        Waypoint(
-                            self.current_pos[0] - 0.5,
-                            self.current_pos[1],
-                            self.imu.getAngle(),
-                            1.5,
-                        ),
-                        self.setup_loading_bay,
-                        self.loading_bay,
-                    ),
-                    self.acceleration,
-                    self.deceleration,
-                )
-            elif self.completed_runs == 2:
-                waypoints = insert_trapezoidal_waypoints(
-                    (self.current_pos, self.setup_loading_bay, self.loading_bay),
-                    self.acceleration,
-                    self.deceleration,
-                )
-            else:
-                self.next_state("stop")
-                return
-            self.pursuit.build_path(waypoints)
-        self.follow_path()
-        if (
-            self.vision.fiducial_in_sight and self.ready_for_vision()
-        ) or self.pursuit.completed_path:
-            self.next_state("intake_hatch")
-
-    @state
-    def intake_hatch(self, initial_call):
-        if initial_call:
-            self.hatch_intake.engage(initial_state="target_tape_align")
-        elif not self.hatch_intake.is_executing:
-            self.next_state("drive_to_cargo_bay")
-
-    @state
-    def stop(self):
-        self.chassis.set_inputs(0, 0, 0)
-        self.done()
-
-    @property
-    def current_pos(self):
-        return Waypoint(
-            self.chassis.odometry_x, self.chassis.odometry_y, self.imu.getAngle(), 2
-        )
-
-    def follow_path(self):
-        vx, vy, heading = self.pursuit.find_velocity(self.chassis.position)
-        if self.pursuit.completed_path:
-            self.chassis.set_inputs(0, 0, 0, field_oriented=False)
-            return
-        # TODO implement a system to allow for rotation in waypoints
-        self.chassis.set_velocity_heading(vx, vy, heading)
-
-    def ready_for_vision(self):
-        if self.pursuit.waypoints[-1][4] - self.pursuit.distance_traveled < 1:
-            return True
-        else:
-            return False
